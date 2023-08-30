36899c8d0ef7c643dcdb96007c43e2f8448e3f06
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-08-30 15:34:29 +0200

    inizialized project with vite+vue, bootstrap, popper, sass, axios

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+// import './style.css'
+import App from './App.vue'
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'popper.js/dist/popper.min.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+createApp(App).mount('#app')
