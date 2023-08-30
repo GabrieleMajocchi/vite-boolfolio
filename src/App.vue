f4fed3a2f0df6f0004682764150bbf6629ed77ac
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-08-30 15:35:06 +0200

    created app.vue with AppMain as component and import of scss

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import AppMain from './components/AppMain.vue';
+export default {
+  name: 'AppVue',
+  components: {
+    AppMain,
+  },
+}
+</script>
+
+<template>
+  <div class="page">
+    <AppMain/>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  @use './styles/partials/mixing.scss' as *;
+  @use './styles/partials/variables.scss' as *;
+  @import './styles/general.scss';
+</style>
