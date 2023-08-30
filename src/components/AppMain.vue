29d8ba78cd963417a8a4d3e503d2bcd830f7c3f4
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-08-30 15:36:10 +0200

    created AppMain.vue with axios call to get projects list from API

---
+++ src/components/AppMain.vue
@@ -0,0 +1,39 @@
+<script>
+import axios from 'axios';
+export default {
+    name: 'AppMain',
+    components: {
+    },
+    data() {
+        return {
+            projects: [],
+            apiUrl: 'http://127.0.0.1:8000/api/projects',
+        }
+    },
+    methods: {
+        getPosts(){
+                axios.get(this.apiUrl)
+                .then((response)=>{
+                    // console.log(response.data.data)
+                    this.projects = response.data.data
+                })
+                .catch(function (error){
+                    console.log(error);
+                })
+            }
+    },
+    created(){
+        this.getPosts()
+    }
+};
+</script>
+
+<template>
+    <main class="d-flex flex-wrap">
+        
+    </main>
+</template>
+
+<style lang="scss" scoped>
+    
+</style>
