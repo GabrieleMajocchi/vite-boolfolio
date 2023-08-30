91abdaf325d2701c53ebba0dabcd177b3880065c
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-08-30 15:37:42 +0200

    bonus added card to show the projects on page

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -30,10 +30,23 @@ export default {
 
 <template>
     <main class="d-flex flex-wrap">
-        
+        <div class="card m-5" v-for="project in projects" >
+            <div class="card-header">
+                <h3>{{project.title}}</h3>
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">Created by: {{project.user.name}}</h5>
+                <p class="card-text">Type: {{project.type.name}}</p>
+                <p>Technologies: <span v-for="tech in project.technologies">{{ tech.name }}&nbsp;</span></p>
+                <p class="card-text">{{project.description.substr(0, 100)}}</p>
+                <a :href="project.link" class="btn btn-primary">See the project site</a>
+            </div>
+        </div>
     </main>
 </template>
 
 <style lang="scss" scoped>
-    
+    .card{
+        width: calc(100% / 5 - 6rem);
+    }
 </style>
